62e6006e08a4d5a342b88337be16e06fccef74f2
itsolver <26458299+itsolver@users.noreply.github.com>
2018-04-30 16:53:23 +1000

    List only 1 product

--- server/orders.js
+++ server/orders.js
@@ -38,7 +38,7 @@ const updateOrder = async (orderId, properties) => {
 
 // List all products.
 const listProducts = async () => {
-  return await stripe.products.list({limit: 3});
+  return await stripe.products.list({limit: 1});
 };
 
 // Retrieve a product by ID.
