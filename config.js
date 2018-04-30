529415e62964a2e7fd30af7872702ba1a8d4403e
itsolver <26458299+itsolver@users.noreply.github.com>
2018-04-30 17:40:36 +1000

    IT Solver config

--- config.js
+++ config.js
@@ -10,12 +10,12 @@ require('dotenv').config();
 
 module.exports = {
   // Default country for the checkout form.
-  country: 'US',
+  country: 'AU',
 
   // Store currency.
   // Note: A few payment methods like iDEAL or SOFORT only work with euros,
   // so it's a good common denominator to test both Elements and Sources.
-  currency: 'eur',
+  currency: 'aud',
 
   // Configuration for Stripe.
   // API Keys: https://dashboard.stripe.com/account/apikeys
@@ -24,9 +24,9 @@ module.exports = {
   // You can fill them in your own `.env` file.
   stripe: {
     // The two-letter country code of your Stripe account (required for Payment Request).
-    country: 'US',
+    country: 'AU',
     // API version to set for this app (Stripe otherwise uses your default account version).
-    apiVersion: '2018-02-06',
+    apiVersion: '2018-02-28',
     // Use your test keys for development and live keys for real charges in production.
     // For non-card payments like iDEAL, live keys will redirect to real banking sites.
     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
