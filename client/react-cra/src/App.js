9c8b4e6176d2b1d6c4c41e6e10be49419bca35cc
Thorsten Schaeff <thorsten@stripe.com>
2020-02-01 19:19:00 +0800

    Proxy API requests to node server.

--- client/react-cra/src/App.js
+++ client/react-cra/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState();
+  useEffect(_ => {
+    (async _ => {
+      const res = await fetch('/products').then(res => res.json());
+      setProducts(res.data);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{products?.length ?? 'loading...'}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
