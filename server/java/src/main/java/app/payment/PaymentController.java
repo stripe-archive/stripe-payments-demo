a087b62ccd8108e4fb874312ef681ea9878d1767
James Willock <willock@stripe.com>
2020-07-29 11:39:00 +0100

    Add `last_payment_error` to Java backend

--- server/java/src/main/java/app/payment/PaymentController.java
+++ server/java/src/main/java/app/payment/PaymentController.java
@@ -57,6 +57,11 @@ public class PaymentController {
         Map<String, Object> status = new HashMap<>();
         status.put("status", paymentIntent.getStatus());
 
+        
+        if (paymentIntent.getLastPaymentError() != null) {
+            status.put("last_payment_error", paymentIntent.getLastPaymentError().getMessage());
+        }
+        
         Map<String, Object> wrapper = new HashMap<String, Object>();
         wrapper.put("paymentIntent", status);
 
